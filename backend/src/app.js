7ae8f06042108a84518c5089ad148d494aab8fa2
Akash God <gaurakash014@gmail.com>
2026-01-28 13:05:10 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "*",
+        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
         credentials: true,
     })
 );
