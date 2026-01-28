6d098492359bf1fedd893a7fd282bbbe80aee626
Akash God <gaurakash014@gmail.com>
2026-01-28 20:10:40 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,8 +10,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://business-startup-lac.vercel.app/",
-        ],
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
